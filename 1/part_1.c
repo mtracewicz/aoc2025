b13732f1588ef6c4fc84118c74047f0594ab7108
Michał Tracewicz <m.tracewicz@gmail.com>
2025-12-02 22:06:47 +0100

    tidy: Formatting

--- 1/part_1.c
+++ 1/part_1.c
@@ -1,46 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-    if(argc > 2){
-	fprintf(stderr, "Invalid argument\n");
-	exit(1);
-    }
-    
-    char* file = (argc == 2) ? argv[1] :"input.txt";
-    FILE* f = fopen(file, "r");
-    if(f == NULL){
-	fprintf(stderr, "Could not open an input file\n");
-	exit(1);
-    }
-
-    int result = 0;
-    char rotation;
-    int move;
-    int position = 50;
-    while(fscanf(f, "%c%d\n", &rotation, &move) == 2 ){
-	if(rotation == 'L'){
-	    position-=move;
-	}else{
-	    position+=move;
+int main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		fprintf(stderr, "Invalid argument\n");
+		exit(1);
 	}
 
-	while(position < 0){
-	    position = 100 + position;
+	char *file = (argc == 2) ? argv[1] : "input.txt";
+	FILE *f = fopen(file, "r");
+	if (f == NULL)
+	{
+		fprintf(stderr, "Could not open an input file\n");
+		exit(1);
 	}
 
-	if(position >= 100){
-	    position%=100;
-	}
+	int result = 0;
+	char rotation;
+	int move;
+	int position = 50;
+	while (fscanf(f, "%c%d\n", &rotation, &move) == 2)
+	{
+		if (rotation == 'L')
+		{
+			position -= move;
+		}
+		else
+		{
+			position += move;
+		}
 
-	#ifdef DEBUG
+		while (position < 0)
+		{
+			position = 100 + position;
+		}
+
+		if (position >= 100)
+		{
+			position %= 100;
+		}
+
+#ifdef DEBUG
 		printf("%c%d -- %d\n", rotation, move, position);
-	#endif
-	
-	if(position == 0){
-	    result++;
+#endif
+
+		if (position == 0)
+		{
+			result++;
+		}
 	}
-    }
-    printf("%d\n", result);
-    fclose(f);
+	printf("%d\n", result);
+	fclose(f);
 }
--- 1/part_2.c
+++ 1/part_2.c
@@ -1,54 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-    if(argc > 2){
-	fprintf(stderr, "Invalid argument\n");
-	exit(1);
-    }
-    
-    char* file = (argc == 2) ? argv[1] :"input.txt";
-    FILE* f = fopen(file, "r");
-    if(f == NULL){
-	fprintf(stderr, "Could not open an input file\n");
-	exit(1);
-    }
-
-    int result = 0;
-    char rotation;
-    int move;
-    int position = 50;
-
-    while(fscanf(f, "%c%d\n", &rotation, &move) == 2 ){
-	#ifdef DEBUG
-	    printf("%c%d -- %d\n", rotation, move, position);
-	#endif
-	
-	int previous_zero = position == 0;
-
-	result+=(move/100);
-	move%=100;
-	position += (rotation == 'R') ? move : -move;
-
-	int swaped = 0;
-	if(position < 0){
-	    position+=100;
-	    swaped = 1;
+int main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		fprintf(stderr, "Invalid argument\n");
+		exit(1);
 	}
 
-	if(position > 99){
-	    position-=100;
-	    swaped = 1;
+	char *file = (argc == 2) ? argv[1] : "input.txt";
+	FILE *f = fopen(file, "r");
+	if (f == NULL)
+	{
+		fprintf(stderr, "Could not open an input file\n");
+		exit(1);
 	}
 
-	if(position == 0 || (!previous_zero && swaped == 1)){
-	    #ifdef DEBUG
-		printf("Add: %c%d\n", rotation, move);
-	    #endif
-	    result++;
-	}
-    }
+	int result = 0;
+	char rotation;
+	int move;
+	int position = 50;
 
-    printf("%d\n", result);
-    fclose(f);
+	while (fscanf(f, "%c%d\n", &rotation, &move) == 2)
+	{
+#ifdef DEBUG
+		printf("%c%d -- %d\n", rotation, move, position);
+#endif
+
+		int previous_zero = position == 0;
+
+		result += (move / 100);
+		move %= 100;
+		position += (rotation == 'R') ? move : -move;
+
+		int swaped = 0;
+		if (position < 0)
+		{
+			position += 100;
+			swaped = 1;
+		}
+
+		if (position > 99)
+		{
+			position -= 100;
+			swaped = 1;
+		}
+
+		if (position == 0 || (!previous_zero && swaped == 1))
+		{
+#ifdef DEBUG
+			printf("Add: %c%d\n", rotation, move);
+#endif
+			result++;
+		}
+	}
+
+	printf("%d\n", result);
+	fclose(f);
 }
